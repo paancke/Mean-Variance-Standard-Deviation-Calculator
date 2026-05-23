1447e1f297f1e0a5dc3bbfcff9503f36d8e0dfdc
Geraldyne <gdynechen@gmail.com>
2026-05-23 22:07:26 +0800

    Add mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -3,12 +3,18 @@ import numpy as np
 def calculate(lst):
     if len(lst) != 9:
         raise ValueError('List must contain nine numbers.')
-    a = np.array([lst[:3], lst[3:6], lst[6:]])
-    def helper(array):
-        return [array(a, axis=0).tolist(), array(a, axis=1).tolist(), array(a).tolist()]
-    return {'mean': helper(np.mean),
-           'variance': helper(np.var),
-           'standard deviation': helper(np.std),
-           'max': helper(np.max),
-           'min': helper(np.min),
-           'sum': helper(np.sum)}
+    matrix = np.array(lst).reshape(3, 3)
+    def helper(func):
+        return [
+            func(matrix, axis=0).tolist(),  #axis 0 = columns
+            func(matrix, axis=1).tolist(),  #axis 1 = rows
+            func(matrix).tolist()           #flattened array
+        ]
+    return {
+        'mean': helper(np.mean),
+        'variance': helper(np.var),
+        'standard deviation': helper(np.std),
+        'max': helper(np.max),
+        'min': helper(np.min),
+        'sum': helper(np.sum)
+    }
